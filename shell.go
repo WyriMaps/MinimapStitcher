32db8f1106da6b8bde1e805d0f2c9579a0c3dabe
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-05-14 00:42:50 +0200

    Fully working stitcher, next up: make it a library

--- shell.go
+++ shell.go
@@ -2,29 +2,35 @@ package main
 
 import (
 	"fmt"
-	//"os"
+	"os"
+	"log"
 	"io/ioutil"
 	"strings"
 	"strconv"
+	"image/draw"
+	"image"
+	"image/png"
 )
 
 const (
-	BASE_DIR = "./Minimaps/"
+	SRC_DIR = "./Minimaps/"
+	DST_DIR = "./StitchedMaps/"
+	TILE_SIZE = 256
 )
 
 func main() {
-	files, _ := ioutil.ReadDir(BASE_DIR)
+	files, _ := ioutil.ReadDir(SRC_DIR)
 	for _, f := range files {
-		compileMinimap(BASE_DIR + f.Name(), f.Name(), false)
+		compileMinimap(DST_DIR + f.Name(), f.Name(), false)
 	}
 }
 
 func compileMinimap(resultFileName string, minimapName string, noLiquid bool) {
 	var foundNoLiquid = false
 	var tiles = make(map[string]string);
-	files, _ := ioutil.ReadDir(BASE_DIR + minimapName)
+	files, _ := ioutil.ReadDir(SRC_DIR + minimapName)
 	for _, f := range files {
-		var fullFileName = BASE_DIR + minimapName + "/" + f.Name()
+		var fullFileName = SRC_DIR + minimapName + "/" + f.Name()
 		if strings.Contains(f.Name(), ".png") {
 			var fName = strings.TrimRight(f.Name(), ".png")
 			var fNameNoLiquid = strings.Contains(fName, "noLiquid")
@@ -51,22 +57,9 @@ func compileMinimap(resultFileName string, minimapName string, noLiquid bool) {
 }
 
 func buildMinimap(resultFileName string, tiles map[string]string)  {
-	//fmt.Printf("%v", tiles)
-	fmt.Println("")
-	fmt.Printf("%v", resultFileName)
-	fmt.Println("")
 	var hc, lc, hr, lr = calculateMinimapSize(tiles)
-	fmt.Printf("%v", hc)
-	fmt.Println("")
-	fmt.Printf("%v", lc)
-	fmt.Println("")
-	fmt.Printf("%v", hr)
-	fmt.Println("")
-	fmt.Printf("%v", lr)
-	fmt.Println("")
-	var files = calculateMinimapTilePlacement(tiles, hc, lc, hr, lr)
-	fmt.Printf("%v", files)
-	fmt.Println("")
+	var files, width, height = calculateMinimapTilePlacement(tiles, hc, lc, hr, lr)
+	createMinimapImage(resultFileName, width, height, files)
 }
 
 func calculateMinimapSize(tiles map[string]string) (hc, lc, hr, lr int) {
@@ -94,31 +87,55 @@ func calculateMinimapSize(tiles map[string]string) (hc, lc, hr, lr int) {
 	return
 }
 
-func calculateMinimapTilePlacement(tiles map[string]string, hc int, lc int, hr int, lr int) (map[string]string) {
+func calculateMinimapTilePlacement(tiles map[string]string, hc int, lc int, hr int, lr int) (map[string]string, int, int) {
 	var files = make(map[string]string)
 
 	var width = 0;
 	var height = 0;
 
 	for i := hc; i < lc; i++ {
-		width += 256
+		width += TILE_SIZE
 		for j := hr; j < lr; j++ {
 			if i == hc {
-				height += 256
+				height += TILE_SIZE
 			}
 
 			var si = strconv.Itoa(i)
 			var sj = strconv.Itoa(j)
-			fmt.Printf("%v", si + "_" + sj)
-			if _, ok := tiles[si + "_" + sj]; !ok {
-				files[si + "_" + sj] = tiles[si + "_" + sj]
-			} else {
-				files[si + "_" + sj] = "empty.png"
+			var oi = strconv.Itoa((i - hc) * TILE_SIZE)
+			var oj = strconv.Itoa((j - hr) * TILE_SIZE)
+			if _, ok := tiles[si + "_" + sj]; ok {
+				files[oi + "_" + oj] = tiles[si + "_" + sj]
 			}
-
-			fmt.Printf("%v", files)
 		}
 	}
 
-	return files
+	return files, width, height
+}
+
+func createMinimapImage(resultFileName string, width int, height int, files map[string]string) {
+	m := image.NewRGBA(image.Rect(0, 0, width, height))
+	for coords, fileName := range files {
+		var coordParts = strings.Split(coords, "_")
+		var x, _ = strconv.Atoi(coordParts[0])
+		var y, _ = strconv.Atoi(coordParts[1])
+
+		tile, err := os.Open(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer tile.Close()
+
+		tileImage, err := png.Decode(tile)
+		if err != nil {
+			fmt.Printf("%v", fileName)
+			log.Fatal(err)
+		}
+
+		draw.Draw(m, image.Rect(x, y, x + TILE_SIZE, y + TILE_SIZE), tileImage, image.Point{0,0}, draw.Src)
+	}
+
+	toimg, _ := os.Create(resultFileName + ".png")
+	png.Encode(toimg, m)
+	defer toimg.Close()
 }
